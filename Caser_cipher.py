ab43d18acaf81e2801454f74cf62c5ea944faaa6
kenutd23 <xavierkenneth4@gmail.com>
2025-02-23 11:46:29 -0600

    commit for cipher

---
+++ Caser_cipher.py
@@ -0,0 +1,15 @@
+def cipher(text, shift):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        elif not char.isalpha():
+            result += char
+    return result
+
+
+print(cipher("my name is kenneth2233 333", 7))
+
